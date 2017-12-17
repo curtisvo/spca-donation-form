37c1cb73636617f94375042267c83b0b6fe3f2de
Curtis van Osch <curtis.van-osch@hec.ca>
2017-12-17 18:50:00 -0500

    add donate form validation js

---
+++ donateFormValidation.js
@@ -0,0 +1,14 @@
+$('#bboxdonation_billing_txtFirstName').change(function() {
+	alert('change');
+});
+
+
+/* for first/last name */
+function cleanName(str) {
+	capitalize(str.trim());
+}
+
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase;
+}
+
