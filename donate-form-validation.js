6867936136d5e19cbeb6d69e9e0bd8be1c888304
Curtis van Osch <curtis.van-osch@hec.ca>
2017-12-17 19:00:59 -0500

    define $ for wordpress

--- donate-form-validation.js
+++ donate-form-validation.js
@@ -1,14 +1,18 @@
-$('#bboxdonation_billing_txtFirstName').change(function() {
-	alert('change');
-});
+(function($) {
+	jQuery(document).ready(function($) {
+
+	$('#bboxdonation_billing_txtFirstName').change(function() {
+		alert('change');
+	});
 
 
-/* for first/last name */
-function cleanName(str) {
-	capitalize(str.trim());
+	/* for first/last name */
+	function cleanName(str) {
+		capitalize(str.trim());
+	}
+
+	function capitalize(str) {
+		return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase;
+	}
 }
-
-function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase;
 }
-
